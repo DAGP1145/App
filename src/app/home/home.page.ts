d76f4e2b5cab80feef48f64e9c50a9154bc3d36e
DAGP1145 <diega.gonzalez@duocuc.cl>
2025-01-19 14:44:34 -0300

    Agregue un Scroll, por lo tanto ocupe el html, css y ts para ocuparlo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, } from '@angular/core'; 
-import { IonicModule } from '@ionic/angular';  /* Se agrego */
+import { InfiniteScrollCustomEvent, IonicModule } from '@ionic/angular';  /* Se agrego */
 import { Router } from '@angular/router';  /* Se agrego */
 
 @Component({
@@ -13,11 +13,28 @@ import { Router } from '@angular/router';  /* Se agrego */
 export class HomePage implements OnInit {
 
   curso: { codigo: string; nombre: string } | undefined;
-
+  items: string[]=[];
   constructor(private router: Router) {
 
     const navigation = this.router.getCurrentNavigation();
     this.curso = navigation?.extras.state?.['curso'];
+
+
+    this.generateItems();
+  }
+  private generateItems(){
+    const count = this.items.length + 1;
+    for (let i =0 ; i < 50; i++) {
+      this.items.push(`Alumno ${count + i}`);
+
+    }
+  }
+
+  onIonInfinite(event: InfiniteScrollCustomEvent) {
+    this.generateItems();
+    setTimeout(() => {
+      event.target.complete();
+    }, 500);
   }
 
 
