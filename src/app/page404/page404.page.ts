0ef8a8a491f1fdaef972720573a8beea12284721
DAGP1145 <diega.gonzalez@duocuc.cl>
2025-01-27 23:27:46 -0300

    Router Guard (Según el flujo de su app) Consumo de api y navegación utilizando navigation extras cuando se necesario y cargando la informacion desde la respuesta de la API: Login : La api tiene un login por lo cual deberiamos poder consumir la api y hacer login Obtener los cursos dado el procesor logeado Obtener los alumno segun el curso selecciona Generar Qr se considero: IdCursos o CodigoCurso Sección Fecha , que se obtiene desde el sistema

--- src/app/page404/page404.page.ts
+++ src/app/page404/page404.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router'; // Asegúrate de importar esto
 
 @Component({
   selector: 'app-page404',
   templateUrl: './page404.page.html',
   standalone: true,  /* Se agrego */
-  imports: [IonicModule],  /* Se agrego */
+  imports: [IonicModule, RouterModule],  /* Se agrego */
   styleUrls: ['./page404.page.scss'],
 })
 export class Page404Page implements OnInit {
