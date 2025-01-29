61c63ba41ab81782e79dbe02ae5c0566943b9cd4
dahakth <alaneduardomolina15@gmail.com>
2025-01-29 16:18:22 -0300

    commit de alan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Importa el plugin de la cámara
+import { Camera } from '@ionic-native/camera/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    Camera,  // Añade el plugin de la cámara en los proveedores
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
--- src/app/perfil-alumno/perfil-alumno.page.ts
+++ src/app/perfil-alumno/perfil-alumno.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular'; /* Se agrego */
+import { IonicModule } from '@ionic/angular'; /* Se agregó */
 import { RouterModule } from '@angular/router'; // Asegúrate de importar esto
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx'; // Importa el servicio Camera
 
 
 @Component({
@@ -11,22 +12,43 @@ import { RouterModule } from '@angular/router'; // Asegúrate de importar esto
   styleUrls: ['./perfil-alumno.page.scss'],
 })
 export class PerfilAlumnoPage implements OnInit {
-  /* Se agregaron los elementos como la variable NombreDocente y los elementos dentro del constructor */
   alertButtons = ['Siguiente'];
   fechaHoy: string = new Date().toLocaleDateString();
+  base64Image: string | undefined;
 
-  constructor() { 
-/* 
-   this.nombreDocente = this.router.getCurrentNavigation()?.extras.state?.['nombre']
-    console.log(this.router.getCurrentNavigation()?.extras.state?.['apellido'])
-    console.log(this.router.getCurrentNavigation()?.extras.state?.['edad'])
- */
+  constructor(private camera: Camera) {}
 
+  ngOnInit() {}
 
+  // Método para tomar una foto con la cámara
+  camara() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+    };
+
+    this.camera.getPicture(options).then(
+      (imageData: string) => {
+        this.base64Image = 'data:image/jpeg;base64,' + imageData;
+        console.log('Imagen tomada:', this.base64Image);
+      },
+      (err: any) => {
+        console.log('Error al tomar la foto:', err);
+      }
+    );
   }
 
-  ngOnInit() {
+  limpiar() {
+    this.base64Image = undefined;
   }
 
+  mostrar() {
+    console.log('Mostrar información');
+  }
+
+  actualizar() {
+    console.log('Actualizar información');
+  }
 }
-
